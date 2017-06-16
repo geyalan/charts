005eb9dc436b4715c295ee1eb7ba66ea3a5b2655
geyalan <490312867@qq.com>
2017-06-16 18:10:47 +0800

    charts project

---
+++ js/H5ComponentBase.js
@@ -0,0 +1,35 @@
+/* 基本图文组件对象 */
+var H5ComponentBase = function(name,cxt){
+	var cxt = cxt || {}
+	// 设置唯一ID
+	var id = ('h5_c_'+ Math.random()).replace('.','_')
+	var cls = name + ' h5_component_' + cxt.type
+
+	var component = $('<div class="h5_component '+cls+' h5_component_name_'+name+'" id="'+id+'"></div>')
+	cxt.text && component.text(cxt.text)
+	cxt.width && component.width(cxt.width/2)
+	cxt.height && component.height(cxt.height/2)
+	cxt.css && component.css(cxt.css)
+	cxt.bg && component.css('backgroundImage','url('+cxt.bg+')')
+
+	if(cxt.center === true){
+		component.css({
+			'left':'50%',
+			'marginLeft':(cxt.width/4*-1)+'px', 
+		})
+	}
+
+	component.on('onLoad',function(){
+		component.addClass(cxt+'_load').removeClass(cls+'_leave')
+		cxt.animateIn && component.animate(cxt.animateIn)
+		return false
+	})
+
+	component.on('onLeave',function(){
+		component.addClass(cxt+'_leave').removeClass(cls+'_load')
+		cxt.animateOut && component.animate(cxt.animateOut)
+		return false
+	})
+
+	return component
+}
